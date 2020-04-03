b504dfb794bd60a324ef2361b19a1ba70f642c46
Martin Spusta <lucas.spusta@gmail.com>
2020-04-03 18:07:25 -0400

    Updated Gradle

--- app/src/main/java/itkido/me/ibmwatsontexttospeech/MainActivity.java
+++ app/src/main/java/itkido/me/ibmwatsontexttospeech/MainActivity.java
@@ -2,48 +2,25 @@ package itkido.me.ibmwatsontexttospeech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.ibm.cloud.sdk.core.http.HttpMediaType;
-import com.ibm.cloud.sdk.core.http.Response;
-import com.ibm.cloud.sdk.core.http.ServiceCall;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
-import com.ibm.watson.assistant.v2.Assistant;
-import com.ibm.watson.assistant.v2.model.CreateSessionOptions;
-import com.ibm.watson.assistant.v2.model.DialogNodeOutputOptionsElement;
-import com.ibm.watson.assistant.v2.model.MessageInput;
-import com.ibm.watson.assistant.v2.model.MessageOptions;
-import com.ibm.watson.assistant.v2.model.MessageResponse;
-import com.ibm.watson.assistant.v2.model.RuntimeResponseGeneric;
-import com.ibm.watson.assistant.v2.model.SessionResponse;
-import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneHelper;
-import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
 import com.ibm.watson.developer_cloud.android.library.audio.StreamPlayer;
 import com.ibm.watson.text_to_speech.v1.TextToSpeech;
 import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
 
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private TextToSpeech textToSpeech;
     StreamPlayer streamPlayer = new StreamPlayer();
-    private boolean initialRequest;
-    private boolean permissionToRecordAccepted = false;
-    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-    private static final int RECORD_REQUEST_CODE = 101;
-    private boolean listening = false;
-    private MicrophoneInputStream capture;
-    private MicrophoneHelper microphoneHelper;
-
     TextView txtYourMessage;
     EditText editText;
     Button btnConvert;
@@ -55,12 +32,12 @@ public class MainActivity extends AppCompatActivity {
         txtYourMessage = findViewById(R.id.txtYourMessage);
         editText = findViewById(R.id.editText);
         btnConvert = findViewById(R.id.btnConvert);
-        microphoneHelper = new MicrophoneHelper(this);
 
 
         btnConvert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Snackbar.make(v, "Loading", Snackbar.LENGTH_LONG).show();
                 convertTextToSpeech();
             }
         });
@@ -94,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... params) {
             streamPlayer.playStream(textToSpeech.synthesize(new SynthesizeOptions.Builder()
                     .text(params[0])
-                    .voice(SynthesizeOptions.Voice.EN_US_LISAVOICE)
+                    .voice(SynthesizeOptions.Voice.EN_US_MICHAELV3VOICE)
                     .accept(HttpMediaType.AUDIO_WAV)
                     .build()).execute().getResult());
             return "Did synthesize";
